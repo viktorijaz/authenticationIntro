391c91750baee895128860ce5ad7e257b716da40
viktorija <viktorija11@gmail.com>
2019-02-18 13:56:09 +0100

    initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
 //connect to MongoDB
-mongoose.connect('mongodb://localhost/testForAuth');
+mongoose.connect('mongodb://localhost/nodeauth');
 var db = mongoose.connection;
 
 //handle mongo error
